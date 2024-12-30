e5a11000229eacac775f5e78c0a1e01b168b1f75
The-Magni <dinhquanganh1606@gmail.com>
2024-12-30 10:12:50 +0800

    separate files

---
+++ chess/lichess/templates/lichess/chess.js
@@ -0,0 +1,23 @@
+function createBoard() {
+    let ChessTable = document.createElement('table');
+    for (let i = 0; i < 8; i++) {
+        let tr = document.createElement('tr');
+        for (let j = 0; j < 8; j++) {
+            let td = document.createElement('td');
+            if ((i + j) % 2 == 0) {
+                td.setAttribute('class', 'cell whitecell');
+                tr.appendChild(td);
+            } else {
+                td.setAttribute('class', 'cell blackcell')
+                tr.appendChild(td);
+            }
+            ChessTable.appendChild(tr);
+        }
+    }
+    document.body.appendChild(ChessTable);
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    createBoard();
+})
