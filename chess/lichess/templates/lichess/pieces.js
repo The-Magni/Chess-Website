e9079640983ad7d6f1e7f4f217d2e397baee15b2
The-Magni <dinhquanganh1606@gmail.com>
2025-01-06 23:59:06 +0800

    create blueprint for King object

--- chess/lichess/templates/lichess/pieces.js
+++ chess/lichess/templates/lichess/pieces.js
@@ -1,16 +1,89 @@
 class Piece {
     constructor(white) {
-        this.white = white;
-        this.kill = false;
+        this._white = white;
+        this._kill = false;
+    }
+    get white() {
+        return this._white;
+    }
+    set white(white) {
+        this._white = white;
+    } 
+    get kill() {
+        return this._kill;
+    }
+    set kill(kill=true) {
+        this._kill = kill;
+    }
+}
+
+class King extends Piece {
+    constructor(white) {
+        super(white);
+        this._castlingDone = false;
+    }
+    get castlingDone() {
+        return this._castlingDone;
+    }
+    set castlingDone(castlingDone) {
+        this._castlingDone = castlingDone;
+    }
+
+    canMove(board, start, end) {
+        if (end.piece.white == this.white) {
+            return false;
+        }
+
+        const x = Math.abs(start.row - end.row);
+        const y = Math.abs(start.col - end.col);
+        if (x + y === 1) {
+            // additional check if this move result in the king being attack
+            return true;
+        }
+        return this.isValidCastling(board, start, end);
+    }
+    isValidCastling(board, start, end) {
+        if (this.castlingDone) {
+            return false;
+        }
+
+        // logic to check if any square is being attacked
+    }
+    isCastlingMove(start, end) {
+        // checking if starting and ending position are correct
     }
 }
 
 export class Square {
     constructor(row, col, white, piece=null) {
-        this.row = row;
-        this.col = col;
-        this.white = white;
-        this.piece = piece;
+        this._row = row;
+        this._col = col;
+        this._white = white;
+        this._piece = piece;
+    }
+    get row() {
+        return this._row;
+    }
+    set row(row) {
+        this._row = row;
+    }
+    get col() {
+        return this._col;
+    }
+    set col(col) {
+        this._col = col;
+    }
+    get white() {
+        return this._white;
+    }
+    set white(white) {
+        this._white = white;
+    }
+    get piece() {
+        return this._piece;
+    }
+    set piece(piece) {
+        this._piece = piece;
     }
 }
 
@@ -18,7 +91,6 @@ export class Board {
     constructor() {
         this.grid = this.createBoard();
     }
-
     createBoard() {
         const grid = [];
         for (let i = 0; i < 8; i++) {
